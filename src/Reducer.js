3623639b0cfca50c34490b94c8936ca2dd920116
George Farro <gtfarro@gmail.com>
2016-08-30 12:54:43 -0500

    require babel polyfill for iOS < 9.0 and Android Symbol

--- src/Reducer.js
+++ src/Reducer.js
@@ -14,6 +14,7 @@ import { ActionMap } from './Actions';
 import { assert } from './Util';
 import { getInitialState } from './State';
 import { Platform } from 'react-native';
+require('../../babel/polyfill');
 
 // WARN: it is not working correct. rewrite it.
 function checkPropertiesEqual(action, lastAction) {
